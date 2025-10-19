9ad712db074f6aa3fc49852e013a8bfe81f54567
Bello <jm.soum@outlook.fr>
2025-10-19 22:21:28 +0200

    Add isometric map

---
+++ scripts/TileMap.gd
@@ -0,0 +1,11 @@
+extends TileMap
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var filled_tiles := get_used_cells(0)
+	for filled_tile: Vector2i in filled_tiles:
+		var neighboring_tiles := get_surrounding_cells(filled_tile)
+		for neighbor: Vector2i in neighboring_tiles:
+			if get_cell_source_id(0, neighbor) == -1:
+				set_cell(0,neighbor,1,Vector2i(1,0))
